69552a6a75e08a89d1085ecac3ab6b2c92a2beee
Cesar San <cesanchezesc@gmail.com>
2022-05-25 18:38:25 -0500

    testing front end header

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,35 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { QUERY_ME_TERAPEUTA, TERAPEUTA } from "../utils/queries";
 
 const Nav = () => {
+    const { terapeutaId } = useParams();
+
+    const {loading, data} = useQuery(
+        terapeutaId ? TERAPEUTA : QUERY_ME_TERAPEUTA, {
+            variables: {_id: terapeutaId}
+        }
+    );
+
+    const nombre = data?.me || data?.terapeuta || {};
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+
     return (
-        <div>This is a nav</div>
+        <header className="w-100 d-flex justify-content-between">
+            {( nombre.nombre? (
+                <h1>Psic. {nombre.nombre} </h1>
+            ) : (
+                <h1>Plataforma-Psi</h1>
+            ) )}
+            <nav>
+                Holi
+            </nav>
+        </header>
     )
 }
 
